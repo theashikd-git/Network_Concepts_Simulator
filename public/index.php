258b18cc11acb53beea9bdfd008ba7b939d74354
Ashik Dewan <theashikd@gmail.com>
2026-07-27 13:18:46 +0600

    Add initial HTML structure for index.php

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Network Concepts Simulator</title>
+</head>
+
+<body>
+
+<h1>Network Concepts Simulator</h1>
+
+<p>
+Learning networking through interactive simulations.
+</p>
+
+</body>
+</html>
