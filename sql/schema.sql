0330864a8b923ac8d0ec1155d961fe0fa8ebd8d8
mdriponahmad <riponahmadsagor@gmail.com>
2026-07-28 00:11:58 +0600

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,3 +9,7 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+ALTER TABLE users
+ADD COLUMN role ENUM('student','admin') DEFAULT 'student',
+ADD COLUMN profile_image VARCHAR(255) DEFAULT NULL;
+
