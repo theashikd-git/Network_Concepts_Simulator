7a28f5b0b8439667a7ef5168fa15a1f6aeb2081f
mdriponahmad <riponahmadsagor@gmail.com>
2026-07-28 00:15:44 +0600

    Create register.php

---
+++ public/register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register</title>
+</head>
+
+<body>
+
+<h2>Create Account</h2>
+
+<form method="POST">
+
+<input type="text"
+name="fullname"
+placeholder="Full Name">
+
+<br><br>
+
+<input type="email"
+name="email"
+placeholder="Email">
+
+<br><br>
+
+<input type="password"
+name="password"
+placeholder="Password">
+
+<br><br>
+
+<button>Create Account</button>
+
+</form>
+
+</body>
+</html>
